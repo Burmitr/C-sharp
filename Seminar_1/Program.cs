5a03c2e7c5a62dabda4e7e0994178542544fe37a
Dmitry <burmitr@yandex.ru>
2022-06-24 01:50:22 +0500

    Seminar 1

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введи первое число: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи второе число: ");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 == number1 * number1)
+{
+    Console.Write("Второе число = квадрату первого.");
+}
+else
+{
+    Console.Write("ОШИБКА!!! Второе число НЕ РАВНО квадрату первого.");
+}
