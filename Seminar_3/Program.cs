aabad966ded9407472afac01fbe90e1403bd9037
Dmitry <burmitr@yandex.ru>
2022-06-30 13:53:36 +0500

    Seminar 3

---
+++ Seminar_3/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = { 1, 2, 3, 4, 5 };
+int i = 0;
+string test;
+Console.WriteLine(arr.Length);
+Console.WriteLine(i);
+while (i < arr.Length) ;
+{
+    arr[i] *= i * 2;
+    Console.Write(arr[i]);
+    i++;
+}
